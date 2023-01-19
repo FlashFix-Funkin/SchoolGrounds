780330dc13ea7a5a5dd8513bae794a7d099ddb3f
Rev2-Git <96362136+Rev2-Git@users.noreply.github.com>
2023-01-18 20:30:39 -0500

    Update ClientPrefs.hx

--- source/ClientPrefs.hx
+++ source/ClientPrefs.hx
@@ -32,7 +32,7 @@ class ClientPrefs {
 	public static var hitsoundVolume:Float = 0;
 	public static var pauseMusic:String = 'Tea Time';
 	public static var checkForUpdates:Bool = true;
-	public static var baseGameUI:Bool = true;
+	public static var baseGameUI:String = 'Base Game';
 	public static var gameplaySettings:Map<String, Dynamic> = [
 		'scrollspeed' => 1.0,
 		'scrolltype' => 'multiplicative', 
