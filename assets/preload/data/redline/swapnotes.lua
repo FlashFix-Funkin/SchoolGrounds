0a9325bf5859107998d00bf07f7718045613ca0a
volv <115556838+saturn-volv@users.noreply.github.com>
2023-02-21 09:55:31 +0800

    Update swapnotes.lua

--- assets/preload/data/redline/swapnotes.lua
+++ assets/preload/data/redline/swapnotes.lua
@@ -1,17 +1,7 @@
-
-
-function onUpdate()
-    --Player Notes
-
-    noteTweenX(defaultPlayerStrumX0, 4, defaultPlayerStrumX0 - 650, 0.025)
-    noteTweenX(defaultPlayerStrumX1, 5, defaultPlayerStrumX1 - 650, 0.025)
-    noteTweenX(defaultPlayerStrumX2, 6, defaultPlayerStrumX2 - 650, 0.025)
-    noteTweenX(defaultPlayerStrumX3, 7, defaultPlayerStrumX3 - 650, 0.025)
-
-    --Opponent Notes
-
-    noteTweenX(defaultOpponentStrumX0, 0, defaultOpponentStrumX0 + 650, 0.025)
-    noteTweenX(defaultOpponentStrumX1, 1, defaultOpponentStrumX1 + 650, 0.025)
-    noteTweenX(defaultOpponentStrumX2, 2, defaultOpponentStrumX2 + 650, 0.025)
-    noteTweenX(defaultOpponentStrumX3, 3, defaultOpponentStrumX3 + 650, 0.025)
+function onCreatePost()
+    for i = 0, getProperty('strumLineNotes.members.length') do
+        local name = i >= 4 and 'Opponent' or 'Player'
+        setProperty('strumLineNotes.members['..i..'].x', _G['default'..name..'StrumX'..i%4])
+        setProperty('strumLineNotes.members['..i..'].y', _G['default'..name..'StrumY'..i%4])
+    end
 end
